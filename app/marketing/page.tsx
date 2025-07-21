a79f275dbcc24125a6fbab0955b6ea622960a12c
iamsreehari02 <sreeharivnambiar72@gmail.com>
2025-07-21 09:27:16 +0530

    route fixes

---
+++ app/marketing/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/marketing/Header";
+import HeroSection from "@/components/marketing/Hero-Section";
+import WhyJoinSection from "@/components/marketing/WhyJoin-Section";
+
+export default function MarketingPage() {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <WhyJoinSection />
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import MarketingPage from "./(marketing)/page";
+// import MarketingPage from "./marketing/page";
 
-export default function Home() {
-  return <MarketingPage />;
+// export default function Home() {
+//   return <MarketingPage />;
+// }
+
+import { redirect } from "next/navigation";
+
+export default function HomePage() {
+  redirect("/marketing");
 }
